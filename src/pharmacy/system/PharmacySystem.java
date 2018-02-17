c61f5c42cc03650ce44e1b47b9c21256ce6583be
Ahmed <>
2018-02-18 00:25:07 +0200

    -connected to database(update)

--- src/pharmacy/system/PharmacySystem.java
+++ src/pharmacy/system/PharmacySystem.java
@@ -7,12 +7,16 @@ import java.sql.SQLException;
 
 public class PharmacySystem {
 
-  // static Connection conn=null;  
+   static Connection conn=null;  
+   static String url="jdbc:mysql://localhost:3306/pharmacydb?useSSL=false";
+   static String username="root";
+   static String password="yourPass";
     public static void main(String[] args) {
         try {
-           Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacydb?useSSL=false"
-                    ,"root","yourPass");
+           conn = DriverManager.getConnection(url,username,password);
+                   
             System.out.println("connected !");
+            
         } catch (SQLException ex) {
               System.out.println("SQLException: " + ex.getMessage());
               System.out.println("SQLState: " + ex.getSQLState());
