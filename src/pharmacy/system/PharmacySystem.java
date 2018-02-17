a30644b0eb056ce90ee7b08fb46d206e99661db6
Ahmed <>
2018-02-18 00:03:40 +0200

    -connected to database

--- src/pharmacy/system/PharmacySystem.java
+++ src/pharmacy/system/PharmacySystem.java
@@ -1,11 +1,22 @@
 
 package pharmacy.system;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PharmacySystem {
 
-  
+  // static Connection conn=null;  
     public static void main(String[] args) {
-       
-    }
-    
+        try {
+           Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacydb?useSSL=false"
+                    ,"root","37492237");
+            System.out.println("connected !");
+        } catch (SQLException ex) {
+              System.out.println("SQLException: " + ex.getMessage());
+              System.out.println("SQLState: " + ex.getSQLState());
+              System.out.println("VendorError: " + ex.getErrorCode());
 }
+        }
+    }    
