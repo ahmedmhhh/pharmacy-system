fd3736c7510d919314f18e53d9a33789a0850a5e
Ahmed <>
2018-02-26 20:17:33 +0200

    -create insert method.

--- src/pharmacy/system/ConnectDB.java
+++ src/pharmacy/system/ConnectDB.java
@@ -4,23 +4,35 @@ package pharmacy.system;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class PharmacySystem {
 
+
+public class ConnectDB {
    static Connection conn=null;  
    static String url="jdbc:mysql://localhost:3306/pharmacydb?useSSL=false";
    static String username="root";
-   static String password="yourPass";
-    public static void main(String[] args) {
-        try {
+   static String password="37492237";
+   public void Connected(){
+   try {
            conn = DriverManager.getConnection(url,username,password);
-                   
+                 
             System.out.println("connected !");
             
         } catch (SQLException ex) {
               System.out.println("SQLException: " + ex.getMessage());
               System.out.println("SQLState: " + ex.getSQLState());
               System.out.println("VendorError: " + ex.getErrorCode());
-            }
         }
-    }    
+   }
+   public void insertDB(){
+       Connected();
+       try {
+             Statement st = conn.createStatement(); 
+             st.executeUpdate("INSERT INTO `pharmacydb`.`users`VALUES(1,'123','a@hotmail.com','0109111111','M')");
+       } catch (Exception e) {
+           
+       }
+       
+   } 
+}
---
+++ src/pharmacy/system/InsertUser.java
@@ -0,0 +1,72 @@
+
+package pharmacy.system;
+
+import java.beans.Statement;
+import java.sql.Connection;
+
+public class InsertUser {
+    public String username;
+    public String password;
+    public String email;
+    public String mobile;
+    public char sex;
+
+    public InsertUser(String username, String password, String email, String mobile, char sex) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.mobile = mobile;
+        this.sex = sex;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+    public void insert(String username,String pass,String email,String mobile,char sex){
+        try {
+             ConnectDB connectDB=new ConnectDB();
+             connectDB.Connected();
+             Connection con=ConnectDB.conn;
+             Statement stmt=(Statement) con.createStatement();
+             stmt.execute();
+        } catch (Exception e) {
+            
+        }     
+    }
+}
